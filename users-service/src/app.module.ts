fad0853faa8364892355ade112115e8a3c60fa69
Jobayer Hossain <jobayerhossain977@gmail.com>
2023-05-03 23:09:17 +0600

    staging: Added

--- users-service/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-
-@Module({
-	imports: [],
-	controllers: [UsersController],
-	providers: [],
-})
-export class AppModule {}
--- users-service/src/model.ts
+++
@@ -1,3 +0,0 @@
-export class CreateUserDTO {
-	name: string;
-}
